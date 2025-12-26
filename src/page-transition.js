8b56958f80b91f8a60243d0b945e5635d85991bd
kltzqu <kltz4074@gmail.com>
2025-12-26 22:35:08 +0200

    added grid bacrkound

--- src/page-transition.js
+++ src/page-transition.js
@@ -7,18 +7,15 @@ function initLinks() {
     link.onclick = async e => {
       e.preventDefault();
 
-      // скрытие
       page.classList.add("page-hidden");
       await new Promise(r => setTimeout(r, 500));
 
-      // подгружаем новую страницу
       const res = await fetch(link.href);
       const html = await res.text();
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, "text/html");
       const newPageContent = doc.querySelector("#page").innerHTML;
 
-      // заменяем содержимое
       page.innerHTML = newPageContent;
       page.classList.remove("page-hidden");
 
