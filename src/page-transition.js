2f157c0bc6387aff28bcab3b393bb36459a05a59
kltzqu <kltz4074@gmail.com>
2025-12-27 18:31:57 +0200

    now you can't switch pages when animation is going

--- src/page-transition.js
+++ src/page-transition.js
@@ -1,11 +1,16 @@
 const page = document.getElementById("page");
 
+let AnimationIsGoing = false;
+
 function initLinks() {
   const links = document.querySelectorAll(".link");
 
   links.forEach(link => {
     link.onclick = async e => {
       e.preventDefault();
+
+      if (AnimationIsGoing) return;
+      AnimationIsGoing = true;
 
       page.classList.add("page-hidden");
       await new Promise(r => setTimeout(r, 500));
@@ -15,15 +20,16 @@ function initLinks() {
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, "text/html");
       const newPageContent = doc.querySelector("#page").innerHTML;
-      
+
       page.innerHTML = newPageContent;
 
       page.classList.add("page-show");
       await new Promise(r => setTimeout(r, 500));
 
       page.classList.remove("page-hidden");
-      page.classList.remove("page-show")
+      page.classList.remove("page-show");
 
+      AnimationIsGoing = false;
       initLinks();
     };
   });
