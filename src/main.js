7a3756ea4414054cb9203fc3451f1946ccc48ea0
kltzqu <kltz4074@gmail.com>
2025-12-29 14:41:30 +0200

    by default loads Projects menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 const { invoke } = window.__TAURI__.core;
+
+const res = await fetch("Menus/Projects.html");
+const html = await res.text();
+
+const parser = new DOMParser();
+const doc = parser.parseFromString(html, "text/html");
+
+const page = doc.getElementById("page");
--- src/page-transition.js
+++ src/page-transition.js
@@ -2,38 +2,56 @@ const page = document.getElementById("page");
 
 let AnimationIsGoing = false;
 
+async function loadPage(url, withAnimation = true) {
+  if (AnimationIsGoing) return;
+  AnimationIsGoing = true;
+
+  if (withAnimation) {
+    page.classList.add("page-hidden");
+    await new Promise(r => setTimeout(r, 500));
+  }
+
+  const res = await fetch(url);
+  const html = await res.text();
+
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const newPage = doc.querySelector("#page");
+
+  if (!newPage) {
+    console.error("На странице нет #page");
+    AnimationIsGoing = false;
+    return;
+  }
+
+  page.innerHTML = newPage.innerHTML;
+
+  if (withAnimation) {
+    page.classList.add("page-show");
+    await new Promise(r => setTimeout(r, 500));
+    page.classList.remove("page-hidden");
+    page.classList.remove("page-show");
+  }
+
+  AnimationIsGoing = false;
+  initLinks();
+}
+
 function initLinks() {
   const links = document.querySelectorAll(".link");
 
   links.forEach(link => {
-    link.onclick = async e => {
+    link.onclick = e => {
       e.preventDefault();
-
-      if (AnimationIsGoing) return;
-      AnimationIsGoing = true;
-
-      page.classList.add("page-hidden");
-      await new Promise(r => setTimeout(r, 500));
-
-      const res = await fetch(link.href);
-      const html = await res.text();
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-      const newPageContent = doc.querySelector("#page").innerHTML;
-
-      page.innerHTML = newPageContent;
-
-      page.classList.add("page-show");
-      await new Promise(r => setTimeout(r, 500));
-
-      page.classList.remove("page-hidden");
-      page.classList.remove("page-show");
-
-      AnimationIsGoing = false;
-      initLinks();
+      loadPage(link.getAttribute("href"));
     };
   });
 }
 
-initLinks();
-window.addEventListener("popstate", () => location.reload());
+document.addEventListener("DOMContentLoaded", () => {
+  const startPage = document.querySelector("[data-startPage]")?.dataset.startpage;
+
+  if (startPage) {
+    loadPage(startPage, false);
+  }
+});
